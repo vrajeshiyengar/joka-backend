d49bff4ad6b2834e17e355c9880b4c96b55478d7
Vrajesh Iyengar <vrajesh.iyengar@gmail.com>
2023-02-03 21:28:14 +0530

    dotenv added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const ApiRoutes = require("./routes/api");
 
 const app = express();
@@ -15,29 +15,38 @@ app.use(
 );
 
 app.use("/api", ApiRoutes);
-const mySqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "isg@123##",
-  database: "JokaAuth",
-  port: 3306,
-  multipleStatements: true,
-});
-
 // const mySqlConnection = mysql.createConnection({
 //   host: "localhost",
 //   user: "root",
-//   password: "1234567890",
+//   password: "isg@123##",
 //   database: "JokaAuth",
+//   port: 3306,
 //   multipleStatements: true,
 // });
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+});
+
+const mySqlConnection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+});
 
 mySqlConnection.connect((err) => {
   if (!err) {
     console.log("JokaAuth DB connection established");
   } else {
     console.log("JokaAuth DB connection failed!");
-    console.log(err);
+    console.log("\tError code: ", err.code);
+    console.log("\tError message: ", err.sqlMessage);
+    console.log("\tFatal: ", err.fatal);
   }
 });
 
