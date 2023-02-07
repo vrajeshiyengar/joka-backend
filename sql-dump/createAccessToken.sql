8bbaeb93308a1a2c076412efa6ce6b567525bc1d
Vrajesh Iyengar <vrajesh.iyengar@gmail.com>
2023-02-07 17:33:10 +0530

    adding fullname column to accesstoken table

--- sql-dump/createAccessToken.sql
+++ sql-dump/createAccessToken.sql
@@ -2,6 +2,7 @@ CREATE TABLE `JokaAuth`.`AccessToken` (
   `access_token` VARCHAR(100) NOT NULL,
   `user_id` VARCHAR(100) NOT NULL,
   `email` VARCHAR(100) NULL,
+  `fullname` VARCHAR(100) NULL,
   `created` VARCHAR(25) NOT NULL,
   `expiry` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`access_token`),
