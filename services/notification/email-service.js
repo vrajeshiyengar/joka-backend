7cad7887c8a67c921bffcf712352845da45ef2dc
Nabaneet247 <nabaneetroy88@gmail.com>
2023-02-13 01:58:29 +0530

    Modified email service

--- services/notification/email-service.js
+++ services/notification/email-service.js
@@ -7,17 +7,17 @@ class EmailService {
             service: 'gmail',
             auth: {
                 user: process.env.EMAIL_ID,
-                pass: process.env.EMAIL_PASSWORD,
-                front_end_link: process.env.WW_LINK
+                pass: process.env.EMAIL_PASSWORD
             }
         });
-        this.front_end_link = process.env.WW_LINK;
+        // this.front_end_link = process.env.WW_LINK;
+        this.front_end_link = process.env.PASSWORD_RESET_APP_LINK;
     }
 
     async sendEmailForPasswordReset(receiverEmailId, reset_password_token, front_end_link = this.front_end_link) {
         return new Promise((resolve, reject) => {
 
-            let mail_html = `<br><br><p style="font-size: 18px; font-family:verdana;">Click <a href="${front_end_link}?reset_password_token=${reset_password_token}">here</a> to reset your password for BB/Courseweb</p>`
+            let mail_html = `<br><p style="font-size: 18px; font-family:verdana;">Click <a href="${front_end_link}/${reset_password_token}">here</a> to reset your password for BB/Courseweb</p>`
             const mailOptions = {
                 from: 'noreply@email.iimcal.ac.in',
                 to: utils.isDevMode() ? 'noreply@email.iimcal.ac.in' : receiverEmailId,
