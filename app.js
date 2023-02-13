3a8cd5645596f3f450847f8416ad1a1115c519ae
Nabaneet247 <nabaneetroy88@gmail.com>
2023-02-13 18:29:44 +0530

    Added global error handler

--- app.js
+++ app.js
@@ -24,3 +24,13 @@ app.listen(port, () =>
     `\n\n\n\nWelcome to JokaBackend!!\nDeveloped by ISG, IIM Calcutta\n\n\n\nJokaBackend listening on port ${port}!\n\nProduction Mode: ${!utils.isDevMode()}`
   )
 );
+
+app.use((err, req, res, next) => {
+  if (err) {
+    console.error('****Server Error****\n\n', err.stack);
+    if (!utils.isDevMode()) {
+      return res.status(500).send('Server Error');
+    }
+    return res.status(500).json({ error: 'Server Error', error_stack: err.stack });
+  }
+});
