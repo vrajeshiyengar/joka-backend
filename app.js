eb6243c6850cfc20ee90ee9ddeb5f89c760f5654
Nabaneet247 <nabaneetroy88@gmail.com>
2023-03-05 13:01:55 +0530

    eh

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const bodyParser = require("body-parser");
 const routes = require("./api-routes/routes");
 const app = express();
 const port = process.env.PORT || 3000;
-const cors = require("cors")
-const utils = require("./utils/utils")
+const cors = require("cors");
+const utils = require("./utils/utils");
 const nocache = require("nocache");
-// var multer = require('multer');
-// var upload = multer();
 
 app.use(bodyParser.json());
 app.use(
@@ -17,9 +15,8 @@ app.use(
     extended: false,
   })
 );
-// app.use(upload.array()); 
 
-if (!utils.isProdMode()) app.use(cors())
+if (!utils.isProdMode()) app.use(cors());
 
 app.use(nocache());
 app.set("etag", false);
@@ -34,10 +31,10 @@ app.listen(port, () =>
 
 app.use((err, req, res, next) => {
   if (err) {
-    console.error('****Server Error****\n\n', err.stack);
+    console.error("****Server Error****\n\n", err.stack);
     if (utils.isProdMode()) {
-      return res.status(500).send('Server Error');
+      return res.status(500).send("Server Error");
     }
-    return res.status(500).json({ error: 'Server Error', error_stack: err.stack });
+    return res.status(500).json({ error: "Server Error", error_stack: err.stack });
   }
 });
